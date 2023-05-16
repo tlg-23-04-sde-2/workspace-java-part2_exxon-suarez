e7c3759664c4dcfac03c7beaa4e5082699afe162
exxon suarez <suarez.exxon@gmail.com>
2023-05-16 12:33:14 -0700

    Lab03.1 complete, including @before

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,13 +9,56 @@
 package com.math;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+
+import org.junit.*;
 
 public class CalculatorTest {
+    // business object(s) under test - called a "fixture"
+    private Calculator calc;
 
-  @Test
-  public void testAdd() {
-    Calculator calc = new Calculator();
-    assertEquals(5, calc.add(1, 4));  // expected, actual
-  }
+    @BeforeClass
+    public static void initializeEntireTestRun()  {
+        System.out.println("initializeEntireTestRun");
+    }
+
+    @AfterClass
+    public static void finalizeTestRun() {
+        System.out.println("finalizeTestRun");
+    }
+
+    @Before                     // this runs before every new Test
+    public void setUp() {
+        System.out.println("setUp");
+        calc = new Calculator();    // here we initialize a new Calculator instance ( calc ) ( which occurs BEFORE every Test )
+    }
+
+    @After
+    public void cleanUp() {
+        System.out.println("cleanUp");
+    }
+
+    @Test
+    public void testIsEven() {
+        System.out.println("test is even");
+        assertTrue(calc.isEven(10));        // this calc references the calc created in our setUp method
+        assertFalse(calc.isEven(11));
+
+    }
+
+
+    @Test
+    public void testDivide() {
+        System.out.println("test is divide");
+        assertEquals(2.5, calc.divide(5,2), .001);      // expected, actual and delta ( because of floating point errors)
+    }
+
+
+
+    @Test
+    public void testAdd() {
+        System.out.println("test is add");
+        assertEquals(5, calc.add(1, 4));  // expected, actual
+    }
+
+
 }
