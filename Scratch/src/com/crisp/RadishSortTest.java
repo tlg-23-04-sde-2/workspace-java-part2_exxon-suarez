325bd49e8a90b6be5be43cc2533a22e4a4874949
exxon suarez <suarez.exxon@gmail.com>
2023-05-22 13:55:20 -0700

    Lab07.1 complete exploring add'l JUnit annotations

--- Scratch/src/com/crisp/RadishSortTest.java
+++ Scratch/src/com/crisp/RadishSortTest.java
@@ -1,6 +1,7 @@
 package com.crisp;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 class RadishSortTest {
@@ -20,6 +21,27 @@ class RadishSortTest {
         radishes.sort(new RadishColorComparator());    // pass a new instance of RadishColorComparator to the sort method ( which takes our newly created radishes and compares them by natural color order )
         System.out.println("Color");
         dump(radishes);
+        System.out.println();
+
+        System.out.println("sort by tail length via anonymous comparator");
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Double.compare(r1.getTailLength(), r2.getTailLength());
+            }
+        });
+        dump(radishes);
+        System.out.println();
+
+        System.out.println("Sort by guys on top via anonymous Comparator");
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Integer.compare(r1.getGuysOnTop(), r2.getGuysOnTop());
+            }
+        });
+        dump(radishes);
+        System.out.println();
     }
 
     private static void dump(List<Radish> radishList) {
